97ed01a1ef3962e305549d294f0c9db035069359
Dawit Mekonnen <dawityitagesu@gmail.com>
2023-06-28 07:21:33 +0300

    ask user to uplaod input file and output file

--- convert.py
+++ convert.py
@@ -1,22 +1,41 @@
-import pandas as pd 
+import pandas as pd
+import xml.etree.ElementTree as ET
+from tkinter import Tk, filedialog
 
-import xml.etree.ElementTree as ET 
-
-def excel_to_tally_xml(excel_file,xml_file):
-    df=pd.read_excel(excel_file)
-    root=ET.Element('{http://www.tallysolutions.com/XMLSchema}TALLYMESSAGE')
-    for _,row in df.iterrows():
-        data_element=ET.SubElement(root,'DATA')
+def excel_to_tally_xml():
+    root = Tk()
+    root.withdraw()
     
-        for column_name,value,in row.items():
-        
-            field_element=ET.SubElement(data_element,'FIELD',{'NAME':column_name})
-            field_element.text=str(value)
-    tree=ET.ElementTree(root)
-    with open(xml_file,'wb')as f:
-        tree.write(f,encoding='utf-8',xml_declaration=True)
-        
+    # Ask user to upload Excel file
+    excel_file_path = filedialog.askopenfilename(title="Select Excel File", filetypes=[("Excel Files", "*.xlsx")])
+    
+    if not excel_file_path:
+        print("No file selected. Exiting...")
+        return
+    
+    # Specify the output XML file path
+    xml_file_path = filedialog.asksaveasfilename(title="Save XML File", defaultextension=".xml", filetypes=[("XML Files", "*.xml")])
+    
+    if not xml_file_path:
+        print("No output file path provided. Exiting...")
+        return
+    
+    df = pd.read_excel(excel_file_path)
+    root = ET.Element('{http://www.tallysolutions.com/XMLSchema}TALLYMESSAGE')
+    
+    for _, row in df.iterrows():
+        data_element = ET.SubElement(root, 'DATA')
+    
+        for column_name, value in row.items():
+            field_element = ET.SubElement(data_element, 'FIELD', {'NAME': column_name})
+            field_element.text = str(value)
+    
+    tree = ET.ElementTree(root)
+    
+    with open(xml_file_path, 'wb') as f:
+        tree.write(f, encoding='utf-8', xml_declaration=True)
+    
+    print("XML file created successfully!")
 
-excel_file_path = 'smaple.xlsx'
-xml_file_path = 'output.xml'
-excel_to_tally_xml(excel_file_path, xml_file_path)
+# Call the function to convert Excel to Tally XML
+excel_to_tally_xml()
